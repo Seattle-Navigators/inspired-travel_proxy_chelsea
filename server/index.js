d880963861177f44268faedb24b5cb07e8908d22
Chelsea Schmidt <cschmidt331@gmail.com>
2020-06-30 11:56:37 -0700

    Remove unnecessary config file, eslint fixes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
-
-const app = express();
 const httpProxy = require('http-proxy');
+
 const proxy = httpProxy.createProxyServer();
+const app = express();
 
 const imageMain = 'http://127.0.0.1:3000';
 const experiences = 'http://127.0.0.1:3636';
@@ -19,42 +19,42 @@ app.use('/:productId', express.static(publicDir));
 
 // ==============================IMAGE MAIN=============================
 app.get('/:productId/imageMain/bundle.js', (req, res) => {
-  proxy.web(req, res, {target: imageMain});
+  proxy.web(req, res, { target: imageMain });
 });
 
 app.get('/api/carousels/:id', (req, res) => {
-  proxy.web(req, res, {target: imageMain});
+  proxy.web(req, res, { target: imageMain });
 });
 
 // =============================EXPERIENCES=============================
 app.get('/:productId/exp/bundle.js', (req, res) => {
-  proxy.web(req, res, {target: experiences});
+  proxy.web(req, res, { target: experiences });
 });
 app.get('/:id/exp/api', (req, res) => {
-  proxy.web(req, res, {target: experiences});
+  proxy.web(req, res, { target: experiences });
 });
 
 // =============================BEST NEARBY=============================
 app.get('/:productId/bestNearby/bundle.js', (req, res) => {
-  proxy.web(req, res, {target: bestNearby});
+  proxy.web(req, res, { target: bestNearby });
 });
 
 app.get('/:attractionId/api/nearbyattractions', (req, res) => {
-  proxy.web(req, res, {target: bestNearby});
+  proxy.web(req, res, { target: bestNearby });
 });
 
 // ===============================REVIEWS===============================
 app.get('/:productId/reviewsModule/bundle.js', (req, res) => {
-  proxy.web(req, res, {target: reviews});
+  proxy.web(req, res, { target: reviews });
 });
 app.get('/:productId/api/reviews', (req, res) => {
-  proxy.web(req, res, {target: reviews});
+  proxy.web(req, res, { target: reviews });
 });
 app.patch('/:productId/api/reviews/:reviewId', (req, res) => {
-  proxy.web(req, res, {target: reviews});
+  proxy.web(req, res, { target: reviews });
 });
 app.patch('/:productId/api/reviews/:reviewId/:imageId', (req, res) => {
-  proxy.web(req, res, {target: reviews});
+  proxy.web(req, res, { target: reviews });
 });
 
 // ===============================LISTEN===============================
