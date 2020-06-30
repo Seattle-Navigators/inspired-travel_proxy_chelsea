3fa6d00547253dd0926ec1133cfadd07348591f1
Chelsea Schmidt <cschmidt331@gmail.com>
2020-06-29 21:38:21 -0700

    Clear out experimental files

--- client/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './getModules';
-
-// ReactDOM.render(<App />, document.getElementById('reviews'));
-
-console.log('test index');
